7e7c0cbfdb9cfb6436e58e34a26539376917d37b
Alexey Zhadan <alexey.zhadan@ukad-group.com>
2020-12-24 05:00:11 -0800

    add class StringCalculator and tests
    Description:
    1. created a new class StringCalculator.cs in Calculator project;
    2. created a new xUnit Test project with name Calculator.Tests;
    3. created a new class StringCalculatorTests in Calculator.Test project;
    4. added to StringCalculatorTests three method for test StringCalculator.

---
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+
+namespace Calculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Add_EmptyString_0Returned()
+        {
+            // arrange
+            StringCalculator calculator = new StringCalculator();
+            int expected = 0;
+
+            // act
+            var result = calculator.Add(string.Empty);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Add_StringOf1_1Returned() 
+        {
+            // arrange
+            StringCalculator calculator = new StringCalculator();
+            string input = "1";
+            int expected = 1;
+
+            // act
+            var result = calculator.Add(input);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Add_StringOf1and2_3Returned()
+        {
+            // arrange
+            StringCalculator calculator = new StringCalculator();
+            string input = "1,2";
+            int expected = 3;
+
+            // act
+            var result = calculator.Add(input);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Calculator/StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class StringCalculator
+    {
+        private const string COMMA = ",";
+
+        public int Add(string numbers)
+        {
+            string[] arrayOfStringNumbers;
+            int resultSum = 0;
+            int countOfNumbers;
+
+            if (numbers == string.Empty)
+            {
+                return resultSum;
+            }
+
+            arrayOfStringNumbers = numbers.Split(COMMA, StringSplitOptions.RemoveEmptyEntries);
+            countOfNumbers = arrayOfStringNumbers.Length;
+
+            if (countOfNumbers > 2)
+            { 
+                countOfNumbers = 2;
+            }
+
+            for(int i = 0; i < countOfNumbers; i++)
+            {
+                resultSum += int.Parse(arrayOfStringNumbers[i]);
+            }
+
+            return resultSum;
+        }
+    }
+}
